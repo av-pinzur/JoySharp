814da1b3b665935a039522fa432248d3853ee9ea
Av Pinzur <av@avpinzur.com>
2014-08-30 18:38:51 -0500

    Added F`1 samples to FTest.

--- Joy.Test/FTest.cs
+++ Joy.Test/FTest.cs
@@ -11,6 +11,8 @@ namespace AvP.Joy.Test
         {
             Func<int, int> factorial = n => F.Loop<int, int, int>(n, 1, loop => (cnt, acc) => cnt == 0 ? loop.Complete(acc) : loop.Recur(cnt - 1, acc * cnt));
             Assert.AreEqual(120, factorial(5));
+
+            Assert.AreEqual(120, F<int>.Loop(5, 1, loop => (cnt, acc) => cnt == 0 ? loop.Complete(acc) : loop.Recur(cnt - 1, acc * cnt)));
         }
 
         [TestMethod]
@@ -18,6 +20,8 @@ namespace AvP.Joy.Test
         {
             Func<int, int> factorial = F.Y<int, int>(self => n => n == 0 ? 1 : n * self(n - 1));
             Assert.AreEqual(120, factorial(5));
+
+            Assert.AreEqual(120, F<int>.YEval(5, self => n => n == 0 ? 1 : n * self(n - 1)));
         }
     }
 }
