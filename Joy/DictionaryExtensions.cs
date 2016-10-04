45883d99c7217777d75315c32c2b60affc1ff948
Av Pinzur <av@avpinzur.com>
2016-10-03 23:34:07 -0500

    Added DictionaryExtensions.GetOrDefault.

--- Joy/DictionaryExtensions.cs
+++ Joy/DictionaryExtensions.cs
@@ -7,6 +7,9 @@ namespace AvP.Joy
     {
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> valueGetter)
         {
+            if (null == dictionary) throw new ArgumentNullException(nameof(dictionary));
+            if (null == valueGetter) throw new ArgumentNullException(nameof(valueGetter));
+
             TValue value;
             if (!dictionary.TryGetValue(key, out value))
             {
@@ -14,6 +17,16 @@ namespace AvP.Joy
                 dictionary.Add(key, value);
             }
             return value;
+        }
+
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue fallback = default(TValue))
+        {
+            if (null == dictionary) throw new ArgumentNullException(nameof(dictionary));
+
+            TValue value;
+            return dictionary.TryGetValue(key, out value)
+                ? value
+                : fallback;
         }
     }
 }
